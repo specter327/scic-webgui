c1dde84203a3b05959f9adf32504dbd2b9d4cf69
specter327 <danielp.mcontact@gmail.com>
2026-07-18 20:26:59 -0600

    Expose scic-webgui 0.3.0

--- src/scic_webgui/__init__.py
+++ src/scic_webgui/__init__.py
@@ -2,7 +2,7 @@ from .application import SCICWebGUI
 from .config import WebGUIConfig
 from .themes import Theme, ThemePackage, ThemeRegistry
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __all__ = [
     "SCICWebGUI",
